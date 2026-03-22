30a546920a8f071c91987a4df554d768b8ba4162
prp20 <ppprasad112@gmail.com>
2026-03-22 14:11:57 +0530

    chore: consolidate frontend files into frontend/ directory

--- src/store/index.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import syncReducer from './slices/syncSlice';
-
-export const store = configureStore({
-  reducer: {
-    sync: syncReducer,
-  },
-});
